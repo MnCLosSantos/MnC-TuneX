2d613209334aa0d0ff97d16d9ba862f50fa55c53
MnCLosSantos <ccpautomotivegroup@gmail.com>
2025-06-01 05:57:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,25 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'MnC Los Santos'
 
-
 client_scripts {
-'client/*'
+    'client/*'
 }
 
 server_scripts {
-'server/*'
+    'server/*'
 }
 
 shared_scripts {
     '@ox_lib/init.lua',
     'config.lua'
 }
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+ui_page 'html/index.html'
+
 lua54 'yes'
