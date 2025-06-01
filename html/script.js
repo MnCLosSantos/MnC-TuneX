88e7d95ff9e1830644cbba35a57c26e1e02c165d
MnCLosSantos <ccpautomotivegroup@gmail.com>
2025-06-01 21:22:44 +0100

    Ui V2

---
+++ html/script.js
@@ -0,0 +1,37 @@
+const clickSound = document.getElementById('clickSound');
+
+function playClick() {
+  clickSound.currentTime = 0;
+  clickSound.play();
+}
+
+window.addEventListener('message', function (event) {
+  if (event.data.action === 'showUI') {
+    document.body.style.display = 'flex';
+  } else if (event.data.action === 'hideUI') {
+    document.body.style.display = 'none';
+  }
+});
+
+document.getElementById('uploadBtn').addEventListener('click', function () {
+  playClick();
+  fetch(`https://${GetParentResourceName()}/uploadTune`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ apply: true }),
+  });
+});
+
+document.getElementById('closeBtn').addEventListener('click', function () {
+  playClick();
+  document.body.style.display = 'none';
+  fetch(`https://${GetParentResourceName()}/closeUI`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+});
+
